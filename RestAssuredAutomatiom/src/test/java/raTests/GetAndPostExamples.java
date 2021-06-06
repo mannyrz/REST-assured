7198dc72f3dcc45ce5afa136a9121d277406dbfc
MRodriguez <mrodriguez@dos2n.com>
2021-06-06 09:06:22 -0600

    First
    Signed-off-by: MRodriguez <mrodriguez@dos2n.com>

---
+++ RestAssuredAutomatiom/src/test/java/raTests/GetAndPostExamples.java
@@ -0,0 +1,72 @@
+package raTests;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class GetAndPostExamples {
+
+	//@Test
+	public void testGet() {
+		
+		
+		baseURI = "https://reqres.in/api";
+		
+		given().
+			get("/users?page=2").
+		then().
+			statusCode(200).
+			body("data[4].first_name", equalTo("George")).
+			body("data.first_name", hasItems("George", "Rachel"));
+		
+	}
+	
+	@Test
+	public void testPost() {
+		
+//		//Map<K,V> map = new HashMap<k,V>()
+//		Map<String,Object> map = new HashMap<String,Object>();
+//		
+//		map.put("name", "Manuel");
+//		map.put("job", "Teacher");
+//		
+//		System.out.println(map);
+//		
+//		JSONObject myJsonObj = new JSONObject(map);
+		
+		//More simple version
+		@SuppressWarnings("unused")
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Manuel");
+		request.put("job", "Teacher");
+		
+		System.out.println(request.toJSONString());
+		
+		baseURI = "https://reqres.in/api";
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("/users").
+		then().
+			statusCode(201).
+			log().all();
+		
+	}
+	
+	
+}
---
+++ RestAssuredAutomatiom/src/test/java/raTests/PutPatchDeleteExamples.java
@@ -0,0 +1,86 @@
+package raTests;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class PutPatchDeleteExamples {
+	
+	@Test
+	public void testPut() {
+		
+	
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Manuel");
+		request.put("job", "Teacher");
+		
+		System.out.println(request.toJSONString());
+		
+		baseURI = "https://reqres.in";
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+		
+		
+	}
+	@Test
+	public void testPatch() {
+		
+	
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Manuel");
+		request.put("job", "Teacher");
+		
+		System.out.println(request.toJSONString());
+		
+		baseURI = "https://reqres.in";
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+		
+		
+	}
+	
+	
+	@Test
+	public void testDelete() {
+		
+			
+		baseURI = "https://reqres.in";
+		
+		when().
+			delete("/api/users/2").
+		then().
+			statusCode(204).
+			log().all();
+		
+		
+	}
+
+}
